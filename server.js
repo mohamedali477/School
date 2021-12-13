fcc8950bc730e56fe5b2e34bf6182be7c68c42b2
mohamedali477 <mohamedali4880@gmail.com>
2021-12-13 09:53:03 +0200

    10 th commint

--- server.js
+++ server.js
@@ -5,4 +5,7 @@ app.use(express.static(__dirname + '/dist/DemoApp'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/DemoApp/index.html'));});
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
